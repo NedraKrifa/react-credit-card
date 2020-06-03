407bc7490ce9143abd2cdb9cf6ef465d2d4a705f
Nedra Krifa <nedrakrifa@gmail.com>
2020-06-03 18:34:13 +0200

    Updates

---
+++ precache-manifest.8566243bfbbbbe8c53971b91807a4a02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0406598e7ba673495e4cd176ba27ee4e",
+    "url": "/react-credit-card/index.html"
+  },
+  {
+    "revision": "be860aace93111f09d72",
+    "url": "/react-credit-card/static/css/main.17f334f2.chunk.css"
+  },
+  {
+    "revision": "f1560d2e768ebdcccfc1",
+    "url": "/react-credit-card/static/js/2.0279f508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-credit-card/static/js/2.0279f508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be860aace93111f09d72",
+    "url": "/react-credit-card/static/js/main.9b234cc7.chunk.js"
+  },
+  {
+    "revision": "4dcb88b5d92970a9437c",
+    "url": "/react-credit-card/static/js/runtime-main.c26e0e4c.js"
+  }
+]);
